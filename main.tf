152013a8fb859415bb2d12a8f6e026147256b0f5
Derek Boateng <dampad0781@gmail.com>
2024-05-16 14:51:16 -0400

    updated main.tf key_name to devops-kp

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ resource "aws_instance" "name" {
   ami             = data.aws_ami.linux2.id
   instance_type   = "t2.medium"
   security_groups = [aws_security_group.jenkins.name]
-  key_name        = "kingsleykeypair"
+  key_name        = "devops-kp"
 
   tags = {
-    Name = "jenkins-zikora"
+    Name = "jenk-0515"
   }
   user_data = file("jenkins-install.sh")
 }
